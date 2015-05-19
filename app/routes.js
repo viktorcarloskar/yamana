edccf9d405433e90819524ec97a470501d6bfffc
Viktor Gustafsson <viktorgustafsson92@gmail.com>
2015-05-19 22:39:13 +0200

    Last id get bug

--- app/routes.js
+++ app/routes.js
@@ -110,7 +110,7 @@ module.exports = function(app, passport, server, io) {
 	// CALLBACK INSTAGRAM
 	// =================================
 	app.get('/callback/instagram/', controllers.viewer.igHandshake);
-	app.post('/callback/instagram/:id/', controllers.viewer.igHandshake);
+	app.get('/callback/instagram/:id', controllers.viewer.igHandshake);
 
 	// =================================
 	// CALLBACK INSTAGRAM
