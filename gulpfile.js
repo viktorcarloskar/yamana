89ce58c1f61fd9f2d303e252a184271a7f9394a0
Viktor Gustafsson <viktorgustafsson92@gmail.com>
2015-05-21 20:27:51 +0200

    Added gulp support

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var nodemon = require('gulp-nodemon');
+
+gulp.task('less', function () {
+  gulp.src('public/css/less/style.less')
+    .pipe(less())
+    .pipe(gulp.dest('public/css'));
+});
+
+gulp.task('start', function () {
+  nodemon({
+    script: 'app.js'
+  , ext: 'js html'
+  , env: { 'NODE_ENV': 'development' }
+  })
+})
+
+gulp.task('default', function() {
+	gulp.run('less');
+	gulp.start('start')
+  	var watchLess = gulp.watch('public/css/less/*.less', ['less']);
+});
